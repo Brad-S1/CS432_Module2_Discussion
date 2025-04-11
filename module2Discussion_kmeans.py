16662dc674494ce2470d08af7db0fbff2f1ac25f
Brad <114127564+Brad-S1@users.noreply.github.com>
2025-04-10 22:39:52 -0700

    remove qualitative columns and instantiate KMeans

--- module2Discussion_kmeans.py
+++ module2Discussion_kmeans.py
@@ -7,6 +7,7 @@ Created on Thu Apr 10 22:16:33 2025
 """
 
 import pandas as pd
+from sklearn.cluster import KMeans
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -25,8 +26,21 @@ print("Number of columns:", len(original_df.columns))
 print("\nColumn names:")
 print(original_df.columns.tolist())
 
-correctDtypes_df = original_df.copy()
 # Set the qualitative variables as data type 'category.
+correctDtypes_df = original_df.copy()
 correctDtypes_df['region'] = correctDtypes_df['region'].astype('category')
 correctDtypes_df['label'] = correctDtypes_df['label'].astype('category')
 print(correctDtypes_df.dtypes)
+
+# drop qualitative columns
+quant_df = correctDtypes_df.copy()
+quant_df=quant_df.drop(['label', 'region'], axis=1)
+print(quant_df)
+
+## Instatiate kmeans
+kmeans_objectCustomers = KMeans(n_clusters=3, max_iter=100)
+kmeans_customers = kmeans_objectCustomers.fit(quant_df)
+
+# get labels
+labels_customers = kmeans_customers.labels_
+print(labels_customers)
